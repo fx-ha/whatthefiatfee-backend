96446826e059c0054158de602547be7aa23a63e1
Felix Ha <felixha@protonmail.com>
2021-10-04 11:51:58 +0200

    feat: :sparkles: trigger isr for fee history

--- src/workers/save-new-fees.ts
+++ src/workers/save-new-fees.ts
@@ -1,7 +1,6 @@
-import axios from 'axios'
 import { getConnection } from 'typeorm'
 import { Fee } from '../entities'
-import { getFeeTable } from '../utils'
+import { getFeeTable, triggerIsr } from '../utils'
 
 const saveNewFees = async (): Promise<void> => {
   const feeTable = await getFeeTable()
@@ -16,11 +15,7 @@ const saveNewFees = async (): Promise<void> => {
       .values(feeTable)
       .execute()
 
-    const { status } = await axios.get('https://whatthefiatfee.vercel.app/')
-
-    if (status === 200) {
-      console.log('trigger Incremental Static Regeneration on frontend')
-    }
+    await triggerIsr('https://whatthefiatfee.vercel.app/')
   }
 }
 
